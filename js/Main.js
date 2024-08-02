24ccb7355d921b5281fd9e764c348af1f007cf3e
TobiasLopezD <entreprisetobys@outlook.com>
2024-08-01 21:11:28 -0600

    Feat: Entregable 6

--- js/Main.js
+++ js/Main.js
@@ -1,19 +1,10 @@
 import { MostrarHTML } from "/js/CrearHTML.js";
+import { MostrarDatosEnConsola } from "/js/ConsultarJson.js";
 
 // Componentes 
 document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
-    MostrarDatosConfiguracion()
+    MostrarDatosEnConsola()
 })
 
-function MostrarDatosConfiguracion() {
-    // Truco (Fetch)
-    let url = "/js/Configuraciones.json";
-    fetch(url)
-    
-        .then(response => response.json())
-    
-        .then(data => console.log(data));
-    
-        
-}
+
