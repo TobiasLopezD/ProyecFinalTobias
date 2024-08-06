cbcbc4e73fe87c5d06e07052338ddd60607de243
TobiasLopezD <entreprisetobys@outlook.com>
2024-08-05 23:02:18 -0600

    Feat:Tarea JS 5

--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,13 @@
 import { MostrarHTML } from "/js/CrearHTML.js";
 import { MostrarDatosEnConsola } from "/js/ConsultarJson.js";
-
+import { Mostrarbanners } from "/js/Banners.js";
+import { Mostrartabla } from "/js/Tabla.js";
 // Componentes 
 document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
     MostrarDatosEnConsola()
+    Mostrarbanners()
+    Mostrartabla ()
 })
 
 
